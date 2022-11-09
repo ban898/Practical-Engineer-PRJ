1f8c544cdf358699cade36acd8e0ebe450d606eb
David Aidlin <davidface2@gmail.com>
2022-11-10 00:10:15 +0200

    Added
    Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import * as React from "react";
-import {
-  RouterProvider,
-  createBrowserRouter,
-  Route,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import WelcomePage from "./pages/WelcomePage";
 import RootLayout from "./pages/RootLayout";
@@ -12,25 +7,47 @@ import Products from "./pages/Products";
 import ErrorElement from "./pages/Error";
 import Login from "./pages/Login";
 
-const myRouter = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />} errorElement={<ErrorElement />}>
-      <Route index element={<WelcomePage />} />
-      <Route path="login" element={<Login />} errorElement={<ErrorElement />} />
-      <Route
-        path="products"
-        element={<Products />}
-        errorElement={<ErrorElement />}
-      />
-    </Route>
-  )
-);
 function App() {
   return (
-    <div>
-      <RouterProvider router={myRouter} />
-    </div>
+    <Routes>
+      <Route path="/" element={<RootLayout />} errorElement={<ErrorElement />}>
+        <Route index element={<WelcomePage />} />
+        <Route
+          path="products"
+          element={<Products />}
+          errorElement={<ErrorElement />}
+        />
+      </Route>
+      <Route path="login" element={<Login />} errorElement={<ErrorElement />} />
+    </Routes>
   );
 }
 
 export default App;
+
+//-------------------------------------------------------------------------------------
+// const myRouter = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<RootLayout />} errorElement={<ErrorElement />}>
+//       <Route index element={<WelcomePage />} />
+//       <Route path="login" element={<Login />} errorElement={<ErrorElement />} />
+//       <Route
+//         path="products"
+//         element={<Products />}
+//         errorElement={<ErrorElement />}
+//       />
+//     </Route>
+//   )
+// );
+
+// function App() {
+//   return (
+//     <div>
+//       <RouterProvider router={myRouter} />
+//     </div>
+//   );
+// }
+
+// RouterProvider,
+// createBrowserRouter,
+// createRoutesFromElements,
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 import classes from "./Login.module.css";
+import imageBg from "../img/loginBG.jpeg";
 
 const Login = () => {
   return (
-    <div className={classes.test}>
-      <p>Test</p>
+    <div className={classes.container}>
+      <div className={classes["login-left"]}>
+        <div className={classes["login-header"]}>
+          <h1>Welcome to our application</h1>
+          <p>Please login to use the platform</p>
+        </div>
+        <form className={classes["login-form"]}>
+          <div className={classes["login-form-content"]}>
+            <div className={classes["form-item"]}>
+              <label htmlfor="email">Enter Email</label>
+              <input type="text" id="email" />
+            </div>
+            <div className={classes["form-item"]}>
+              <label htmlfor="password">Enter Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className={classes["form-item"]}>
+              <div className={classes.checkbox}>
+                <input type="checkbox" id="rememberMeCheckbox" />
+                <label htmlfor="rememberMeCheckbox" id="checkboxLabel">
+                  Remember me
+                </label>
+              </div>
+            </div>
+            <button type="submit">Sign In</button>
+          </div>
+          <div className={classes["login-form-footer"]}>
+            <a href="#">
+              <img width="30" alt="facebook" /> facebook
+            </a>
+            <a href="#">
+              <img width="30" alt="google" />
+              Google
+            </a>
+          </div>
+        </form>
+      </div>
+      <div className={classes["login-right"]}>
+        <img src={imageBg} />
+      </div>
     </div>
   );
 };
