81b6120bd2370a3ac29aa08b8ef01960783d8963
ban898 <davidface2@gmail.com>
2022-12-20 20:30:57 +0200

    Updated Profile Page

--- client/src/pages/Ecom/Profile/ProfilePage.jsx
+++ client/src/pages/Ecom/Profile/ProfilePage.jsx
@@ -30,12 +30,124 @@ const ProfilePage = () => {
               ml={"22%"}
             ></BlueButton>
           </div>
-          <div className={classes.box}>Email</div>
-          <div className={classes.box}>Address</div>
-          <div className={classes.box}>Password</div>
+          <div className={classes.box}>
+            <div className={classes.header}>Email</div>
+            <div className={classes.content}>
+              <TextField
+                id="outlined-basic"
+                label="New Email"
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-basic"
+                label="Confrim Email"
+                variant="outlined"
+              />
+            </div>
+            <BlueButton
+              buttonText={"change"}
+              padding={"5px"}
+              width={"50%"}
+              mt={"1rem"}
+              ml={"22%"}
+            ></BlueButton>
+          </div>
+          <div className={classes.box}>
+            <div className={classes.header}>Password</div>
+            <div className={classes.content}>
+              <TextField
+                id="outlined-basic"
+                label="New Password"
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-basic"
+                label="Confirm Password"
+                variant="outlined"
+              />
+            </div>
+            <BlueButton
+              buttonText={"change"}
+              padding={"5px"}
+              width={"50%"}
+              mt={"1rem"}
+              ml={"22%"}
+            ></BlueButton>
+          </div>
+          <div className={classes.box}>
+            <div className={classes.header}>Address</div>
+            <div className={classes.content}>
+              <TextField id="outlined-basic" label="City" variant="outlined" />
+              <TextField
+                id="outlined-basic"
+                label="Street"
+                variant="outlined"
+              />
+            </div>
+            <BlueButton
+              buttonText={"change"}
+              padding={"5px"}
+              width={"50%"}
+              mt={"1rem"}
+              ml={"22%"}
+            ></BlueButton>
+          </div>
         </div>
         <div className={classes.rightSide}>
-          <div>Orders</div>
+          <div className={classes.accbox}>
+            <div style={{ textAlign: "center", fontSize: "26px" }}>
+              Account Information :
+            </div>
+            <div className={classes.rowContent}>
+              <div className={classes.row}>
+                <div>Current Name :</div>
+                <div className={classes.data}>
+                  <TextField
+                    id="outlined-read-only-input"
+                    label="Api call"
+                    defaultValue="Api call"
+                    InputProps={{
+                      readOnly: true,
+                    }}
+                  />
+                </div>
+              </div>
+              <div className={classes.row}>
+                <div>Current Email :</div>
+                <div className={classes.data}>
+                  <TextField
+                    id="outlined-read-only-input"
+                    label="API CALL"
+                    defaultValue="API CALL"
+                    InputProps={{
+                      readOnly: true,
+                    }}
+                  />
+                </div>
+              </div>
+              <div className={classes.row}>
+                <div>Current Address :</div>
+                <div className={classes.data}>
+                  <TextField
+                    id="outlined-read-only-input"
+                    label="API CALL"
+                    defaultValue="API CALL"
+                    InputProps={{
+                      readOnly: true,
+                    }}
+                    style={{ marginRight: "1rem" }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={classes.orderBox}>
+            <div style={{ textAlign: "center", fontSize: "22px" }}>Orders</div>
+            <div className={classes.singleOrder}>Test</div>
+            <div className={classes.singleOrder}>Test</div>
+            <div className={classes.singleOrder}>Test</div>
+            <div className={classes.singleOrder}>Test</div>
+          </div>
         </div>
       </div>
     </section>
